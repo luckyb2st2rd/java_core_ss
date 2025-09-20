8d28c216512c125be56b2c128e790310a972177f
Artem <cartem33@gmail.com>
2025-09-20 03:06:07 +0300

    Установил CheckStyle и справил ошибки, которые он показывал

--- src/basics/Variables.java
+++ src/basics/Variables.java
@@ -2,8 +2,7 @@ package basics;
 
 public class Variables {
     public static void main(String[] args) {
-        // 1. Обявление и инициализация переменных
-        byte b = 126;
+        byte b = 126;  // 1. Обявление и инициализация переменных
         short s = 180;
         int i = 22124535;
         long l = 921243232;
@@ -12,8 +11,7 @@ public class Variables {
         boolean bool = true;
         char simbol = 'A';
 
-        // 2. Вывод значений с описанием
-        System.out.println("Рост ребенка составляет: " + b);
+        System.out.println("Рост ребенка составляет: " + b);  // 2. Вывод значений с описанием
         System.out.println("Средний рост человека составляет: " + s);
         System.out.println("Население города Z составляет: " + i);
         System.out.println("Население города в Китае составляет: " + l);
@@ -22,17 +20,13 @@ public class Variables {
         System.out.println("Ты лох, и это " + bool);
         System.out.println("Первая буква моего имени, это " + simbol);
 
-        // 3. Явное приведение long в int
-        int i1 = (int) l;
-
+        int i1 = (int) l; // 3. Явное приведение long в int
         System.out.println(i1);
-
         long faild;
         faild = l - i1;
         System.out.println("Потеря данных при конвертации составила: " + faild);
 
-        // 4. Конвертация char в int и обратно
-        int i2 = simbol + 1;
+        int i2 = simbol + 1;  // 4. Конвертация char в int и обратно
         int i3 = simbol + 2;
         int i4 = simbol + 3;
 
@@ -43,7 +37,6 @@ public class Variables {
         char sim1 = (char) i2;
         char sim2 = (char) i3;
         char sim3 = (char) i4;
-
         System.out.println(sim1);
         System.out.println(sim2);
         System.out.println(sim3);
